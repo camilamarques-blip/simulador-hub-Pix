609d7aed85a57b021f6c3ffd7075455989859fd6
camilamarques-blip <camila.marques@willbank.com.br>
2025-10-24 10:31:46 -0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import PixSimulator from './PixSimulator';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <PixSimulator />
-  </React.StrictMode>
-);
