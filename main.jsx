39a3ef873e7c2dde177cb9badc024e1f7aeecc3f
camilamarques-blip <camila.marques@willbank.com.br>
2025-10-24 10:37:23 -0300

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PixSimulator from './PixSimulator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PixSimulator />
+  </React.StrictMode>
+);
